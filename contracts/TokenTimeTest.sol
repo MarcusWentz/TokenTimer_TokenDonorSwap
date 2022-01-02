d310c08ab5a9b3615e47305ba1e5cf7649ebf64b
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-02 17:17:42 -0500

    Update TokenTimeTest.sol

--- contracts/TokenTimeTest.sol
+++ contracts/TokenTimeTest.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenTimeTest is ERC20{
 
     uint public startingBlockTimestamp;
     uint public endingBlockTimestamp;
-    address public TokenTimeTestAddress = address(this);    
+    address public TokenTimeTestAddress = address(this);
     TokenTimeTest tokenObject = TokenTimeTest(TokenTimeTestAddress);
 
     event TransferEvent(
-        uint indexed date,
         address indexed from
     );
 
     constructor() ERC20("TokenTimeTest","TTT") {
         _mint(address(this), 1*(10**18) );
-        startingBlockTimestamp =  block.timestamp + 15; //Open to transfer after 15 seconds [safe after at least 15 seconds from MEV]
-        endingBlockTimestamp = block.timestamp + 60; //Transfer option ends after 60 seconds
+        startingBlockTimestamp =  block.timestamp + 30; //Open to transfer after 30 seconds [15 seconds for block.timestamp helps protect against MEV].
+        endingBlockTimestamp = block.timestamp + 120; //Transfer option ends after 120 seconds
     }
 
     function currentBlockTime() public view returns (uint) {
@@ -26,11 +25,11 @@ contract TokenTimeTest is ERC20{
     }
 
     function timerTransfer() public {
-        require(block.timestamp > startingBlockTimestamp , "Epoch time must be 2 over seconds after contract deployed!");
-        require(block.timestamp < endingBlockTimestamp , "Transfer time expired [60 seconds].");
+        require(block.timestamp > startingBlockTimestamp , "Transfer can start after about 30 seconds after deployed!");
+        require(block.timestamp < endingBlockTimestamp , "Transfer time expired [about 120 seconds after deployment]!");
         require(tokenObject.balanceOf(address(this))  > 0, "All funds have from contract have been moved already!");
-        tokenObject.transfer(msg.sender, 1*(10**18) ); 
-        emit TransferEvent(block.timestamp, msg.sender);
+        tokenObject.transfer(msg.sender, 1*(10**18) );
+        emit TransferEvent(msg.sender);
     }
-    
+
 }
